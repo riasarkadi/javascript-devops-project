c4325dc7ad95938035dfb1dc484b4a441a89f11d
ria <ria.sarkadi@gmail.com>
2023-03-13 20:28:51 +0100

    fix for watcher

--- module_4/task_1/fds-watcher.sh
+++ module_4/task_1/fds-watcher.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 get_disk_space() {
-treshold=$1
+threshold=$1
 
 if [ -z $1 ]; then
-treshold="600"
+threshold="600"
 fi
 
-echo "Disk space treshold: $treshold M"
+echo "Disk space threshold: $threshold M"
 
 df -m | tail -n +2 | grep -vE 'tmpfs' | awk '{print $4 " " $1}' | while read output; do
 
 space=$(echo $output | awk '{print $1}')
 disk=$(echo $output | awk '{print $2}')
 
-if (("$space" < "$treshold"))
+if (("$space" < "$threshold"))
 then
-    echo "Warning! Disk space is below treshold on $disk: $space M"
+    echo "Warning! Disk space is below threshold on $disk: $space M"
 fi
 done
 }
 
-while :; do clear; get_disk_space; sleep 2; done
+while :; do clear; get_disk_space $1; sleep 2; done
